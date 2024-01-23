6c4a147494cf2afbc522ad21ae9e3bbe315b700a
sandro-lucas <sandrolucasekell@gmail.com>
2024-01-23 16:47:57 -0300

    sa

--- page/telaLogin/script.js
+++ page/telaLogin/script.js
@@ -4,17 +4,16 @@ function validateLogin() {
   var loginResult = document.getElementById("loginResult");
 
   // Exemplo simples de validação (usuário: "user", senha: "password")
-  if (username === "lucas" && password === "123456") {
+  if (username === "lucas" && password === "123") {
     loginResult.textContent = "Login successful!";
     loginResult.style.color = "#4caf50";
-
     // Redireciona para a segunda tela após o login bem-sucedido
-    window.location.href = "../telaHome/index.html";
-  }   if (username === "" && password === "") {
-
+    window.location.href  = "../TelaAdm/index.html";
+  }
+  if (username === "" && password === "") {
     loginResult.textContent = "fAVOR PREENCHER OS CAMPOS!";
     loginResult.style.color = "#f44336";
-  }else {
+  } else {
     loginResult.textContent = "Seu login ou senha está errado!";
     loginResult.style.color = "#f44336";
   }
